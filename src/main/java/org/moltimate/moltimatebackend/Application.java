b85667f812ba7dcfa2b26177cd926911c6425d07
blackpan2 <blackpan2@gmail.com>
2019-02-07 14:51:46 -0500

    Add swagger to the Alignment controller

--- src/main/java/org/moltimate/moltimatebackend/Application.java
+++ src/main/java/org/moltimate/moltimatebackend/Application.java
@@ -3,9 +3,11 @@ package org.moltimate.moltimatebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/moltimate/moltimatebackend/controller/AlignmentController.java
+++ src/main/java/org/moltimate/moltimatebackend/controller/AlignmentController.java
@@ -1,5 +1,9 @@
 package org.moltimate.moltimatebackend.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.moltimate.moltimatebackend.request.ActiveSiteAlignmentRequest;
 import org.moltimate.moltimatebackend.request.BackboneAlignmentRequest;
@@ -17,18 +21,35 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/align")
+@Api(value = "/align", description = "Alignment Controller", produces = "application/json")
 @Slf4j
 public class AlignmentController {
 
     @Autowired
     private AlignmentService alignmentService;
 
+    @ApiOperation(value = "Active Site Alignment", response = AlignmentResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "List of active site alignments", response = AlignmentResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 404, message = "Active site not found")
+    })
     @RequestMapping(value = "/activesite", method = RequestMethod.POST)
     public ResponseEntity<AlignmentResponse> activeSiteAlignment(@RequestBody ActiveSiteAlignmentRequest alignmentRequest) {
+        /* TODO: Test case if a bad PDB id is given
+            Ignore bad PDB ids and continue search
+            Collect failed ids and return so a toast can trigger
+            TODO: Throw 404 if all PDB ids are incorrect */
         log.info("Received request to align active sites: " + alignmentRequest);
         return ResponseEntity.ok(alignmentService.alignActiveSites(alignmentRequest));
     }
 
+    @ApiOperation(value = "Backbone alignment", response = AlignmentResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "List of active site alignments", response = AlignmentResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error"),
+            @ApiResponse(code = 404, message = "Active site not found")
+    })
     @RequestMapping(value = "/backbone", method = RequestMethod.POST)
     public ResponseEntity<AlignmentResponse> backboneAlignment(@RequestBody BackboneAlignmentRequest alignmentRequest) {
         log.info("Received request to align active sites: " + alignmentRequest);
