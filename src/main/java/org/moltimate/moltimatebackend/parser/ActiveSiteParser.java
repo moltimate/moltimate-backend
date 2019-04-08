729595b945d28a9f12f7394d18b486a86fdf332d
Steve Teplica <steplica96@gmail.com>
2019-04-08 10:22:58 -0400

    Clean up active site parsing logic

---
+++ src/main/java/org/moltimate/moltimatebackend/parser/ActiveSiteParser.java
@@ -0,0 +1,10 @@
+package org.moltimate.moltimatebackend.parser;
+
+import org.moltimate.moltimatebackend.model.ActiveSite;
+
+import java.util.List;
+
+public interface ActiveSiteParser {
+
+    List<ActiveSite> parseMotifs();
+}
