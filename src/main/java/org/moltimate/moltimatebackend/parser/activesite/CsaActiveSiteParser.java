0cf421a2f1af0a3f20c9a9600a4e4069795db137
kdm5498 <kdm5498@rit.edu>
2019-10-30 03:16:58 -0400

    Both Active Site parsers now pull from the local resources, rather than directly from github. Test motif files were failing due to messed up line endings when pulling from dev, switched from /r/n to just /n.

--- src/main/java/org/moltimate/moltimatebackend/parser/activesite/CsaActiveSiteParser.java
+++ src/main/java/org/moltimate/moltimatebackend/parser/activesite/CsaActiveSiteParser.java
@@ -4,20 +4,18 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import org.moltimate.moltimatebackend.model.ActiveSite;
 import org.moltimate.moltimatebackend.model.Residue;
-import org.springframework.core.io.FileUrlResource;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class CsaActiveSiteParser implements ActiveSiteParser {
 
-    // TODO: don't make web requests for this, use the local file
-    private static final String CSA_CSV_URL = "https://raw.githubusercontent.com/moltimate/moltimate-backend/master/src/main/resources/motifdata/csa_curated_data.csv";
+    private static final String CSA_CSV = "motifdata/csa_curated_data.csv";
 
     /**
      * Retrieve all active sites from the Catalytic Site Atlas.
@@ -26,7 +24,7 @@ public class CsaActiveSiteParser implements ActiveSiteParser {
      */
     public List<ActiveSite> parseMotifs() {
         try {
-            Reader reader = new InputStreamReader(new FileUrlResource(new URL(CSA_CSV_URL)).getInputStream());
+            Reader reader = new InputStreamReader(new ClassPathResource(CSA_CSV).getInputStream());
             CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1)
                     .build();
 
