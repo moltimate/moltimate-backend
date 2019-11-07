ef05779ad9bdbcb37d875aac7b9d075f02077731
kdm5498 <kdm5498@rit.edu>
2019-11-06 20:53:57 -0500

    Removed tests until implementation is complete.

---
+++ src/test/java/org/moltimate/moltimatebackend/service/TestDockingService.java
@@ -0,0 +1,17 @@
+package org.moltimate.moltimatebackend.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TestDockingService {
+	@InjectMocks
+	private DockingService dockingService;
+
+	@Test
+	public void testDocking() {
+		// todo Implement test here
+	}
+}
