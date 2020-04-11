8cf80d574579361aa6e2c778779d6527c1e081e1
Ada Phillips <ada_phillips@mail.rit.edu>
2020-04-10 22:34:25 -0400

    MOL-71: Environment Variable control of container IP addresses (#149)
    * MOL-71: Environment Variable control of container IP addresses
    
    Added env variable specification to application.properties
    Added env_variables.yaml to transfer env variables to appengine
    Added set-env-variables.sh to populate env_variables.yaml from
    build environment env variables

---
+++ set-env-variables.sh
@@ -0,0 +1,9 @@
+if [ ! -z "$AUTODOCK_URL" ]
+then 
+    echo "    AUTODOCK_URL: $AUTODOCK_URL" >> env_variables.yaml
+fi
+
+if [ ! -z "$OPENBABEL_URL" ]
+then 
+    echo "    OPENBABEL_URL: $OPENBABEL_URL" >> env_variables.yaml
+fi
