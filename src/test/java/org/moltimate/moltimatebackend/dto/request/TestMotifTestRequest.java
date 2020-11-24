9275517a893c6223ab09b1647331b881da2750bd
Melissa Gould <mag8224@g.rit.edu>
2020-11-24 13:34:09 -0500

    Added tests for motif files

---
+++ src/test/java/org/moltimate/moltimatebackend/dto/request/TestMotifTestRequest.java
@@ -0,0 +1,29 @@
+package org.moltimate.moltimatebackend.dto.request;
+
+import org.mockito.Mockito;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMotifTestRequest {
+
+    @Test
+    public void testMotifStructure() {
+
+    }
+
+    @Test
+    public void testGetType() {
+
+    }
+    @Test
+    public void testGetPrecisionFactor() {
+
+    }
+    @Test
+    public void TestGetRandomCount() {
+
+    }
+}
