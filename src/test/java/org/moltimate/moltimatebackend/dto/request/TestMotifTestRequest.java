5ae7185083c8cf451185cc14c18c5d1d77776bd6
Melissa Gould <mag8224@g.rit.edu>
2020-11-24 16:49:12 -0500

    Testing MotifTestRequest

--- src/test/java/org/moltimate/moltimatebackend/dto/request/TestMotifTestRequest.java
+++ src/test/java/org/moltimate/moltimatebackend/dto/request/TestMotifTestRequest.java
@@ -1,29 +1,47 @@
 package org.moltimate.moltimatebackend.dto.request;
 
+import org.biojava.nbio.structure.Structure;
 import org.mockito.Mockito;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.moltimate.moltimatebackend.exception.InvalidPdbIdException;
+import org.moltimate.moltimatebackend.model.MotifTest;
 
 public class TestMotifTestRequest {
 
     @Test
     public void testMotifStructure() {
-
+        MotifTestRequest mtr = new MotifTestRequest();
+        mtr.setPdbId("1a0j");
+        Structure struct = mtr.motifStructure();
+        Assert.assertEquals("1A0J", struct.getPDBCode());
     }
 
     @Test
     public void testGetType() {
-
+        MotifTestRequest mtr = new MotifTestRequest();
+        Assert.assertEquals(MotifTestRequest.Type.SELF, mtr.getType());
+        mtr.setType(MotifTestRequest.Type.HOMOLOG);
+        Assert.assertEquals(MotifTestRequest.Type.HOMOLOG, mtr.getType());
     }
+
     @Test
     public void testGetPrecisionFactor() {
-
+        MotifTestRequest mtr = new MotifTestRequest();
+        mtr.setPrecisionFactor(10);
+        Assert.assertEquals(10, mtr.getPrecisionFactor());
+        mtr.setPrecisionFactor(-1);
+        Assert.assertEquals(1, mtr.getPrecisionFactor());
     }
     @Test
     public void TestGetRandomCount() {
-
+        MotifTestRequest mtr = new MotifTestRequest();
+        mtr.setRandomCount(10);
+        Assert.assertEquals(10, mtr.getRandomCount());
+        mtr.setRandomCount(-1);
+        Assert.assertEquals(1, mtr.getRandomCount());
     }
 }
