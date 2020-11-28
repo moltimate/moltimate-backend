dd63cfae44aeb7413027d748e82c1c028a538838
Melissa Gould <mag8224@g.rit.edu>
2020-11-28 11:33:56 -0500

    Testing for MotifAlignmentResponse

--- src/main/java/org/moltimate/moltimatebackend/util/PdbXmlClient.java
+++ src/main/java/org/moltimate/moltimatebackend/util/PdbXmlClient.java
@@ -14,8 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 @Slf4j
 public class PdbXmlClient {
