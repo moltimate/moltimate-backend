775e2c0d46940194f4044f0f27e756ac47d83a19
mev5063 <mev5063@g.rit.edu>
2021-01-29 13:59:11 -0500

    Adding TODOs for what still needs to be done for deployment

--- src/main/java/org/moltimate/moltimatebackend/config/ProdConfiguration.java
+++ src/main/java/org/moltimate/moltimatebackend/config/ProdConfiguration.java
@@ -1,5 +1,6 @@
 package org.moltimate.moltimatebackend.config;
 
+import com.sun.xml.bind.v2.TODO;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.retry.annotation.EnableRetry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("prod")
 public class ProdConfiguration implements WebMvcConfigurer {
 
+//    TODO: SET THIS TO USE ENV VARIABLE + GET NEW DOMAIN.
     private static final String ALLOWED_ORIGIN = "https://moltimate.appspot.com";
 
     @Override
